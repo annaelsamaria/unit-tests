f8fc1a72ac1b7c97805195de82f0d79ee1d1cdc7
Anna Gustafsson <annagustafsson@MacBook-Pro-som-tillhor-Anna.local>
2020-03-02 21:46:06 +0100

    did the first three tests

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,19 @@
 export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
+  if (items.length === 2) {
+    return `First: ${items[0]}, Last: ${items[1]}`
+  }
+
+  if (items.length > 2) {
+    let lastItem = items[items.lenght - 1]
+    return `First: ${items[0]}, Last: ${lastItem}`
+  }
+
+  if (items.length === 1) {
+    return `Only item: ${items[0]}`
+  }
+
+  if (items.length === 0) {
+    return `no items!`
+  }
 }
+
